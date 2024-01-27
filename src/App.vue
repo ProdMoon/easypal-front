46365b4f0fcfc1047f5c43a1dbff8eaeb34c8cee
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2024-01-27 16:10:39 +0900

    feat: simple header

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,11 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <RouterLink to="/">Go Home</RouterLink>
+  <header class="flex items-center justify-center bg-slate-100">
+    <RouterLink class="flex items-end text-2xl font-bold" to="/">
+      <img class="h-10 w-10 mr-3" src="/cattiva.png" alt="Cute Cattiva" />
+      EasyPal
+    </RouterLink>
   </header>
 
   <RouterView />
